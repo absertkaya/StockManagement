c3fe4f61e7a0b9d1371748db9ca72c68a11d7257
Baki Sertkaya <baki.sertkaya@vgd.eu>
2020-03-12 16:58:38 +0100

    filter options itemlist

--- StockManagement/Pages/OverviewPages/ItemList.razor.cs
+++ StockManagement/Pages/OverviewPages/ItemList.razor.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Blazored.Modal;
 using Blazored.Modal.Services;
 using Blazored.Toast.Services;
+using System.Linq;
 
 namespace StockManagement.Pages.OverviewPages
 {
@@ -27,19 +28,34 @@ namespace StockManagement.Pages.OverviewPages
         protected Product _product;
         protected Supplier _supplier;
         protected IList<Item> _items;
-
+        protected IEnumerable<Item> _filteredItems;
+        protected string _filterString = "";
+        protected bool _instock;
         protected override void OnInitialized()
         {
             if (Id != null)
             {
                 _product = (Product)Repository.GetById(typeof(Product), Id);
                 _items = _product.Items;
+                
             }
             if (SupplierId != null)
             {
                 _supplier = (Supplier)Repository.GetById(typeof(Supplier), SupplierId);
                 _items = _supplier.Items;
             }
+            _filteredItems = new List<Item>(_items);
+        }
+
+        protected void Filter()
+        {
+            _filteredItems = _items.Where(i => i.SerialNumber.ToLower().Contains(_filterString.Trim().ToLower()) && i.InStock == _instock);
+        }
+
+        protected void CheckInStock()
+        {
+            _instock = !_instock;
+            Filter();
         }
 
         protected void DeleteItem(Item item)
